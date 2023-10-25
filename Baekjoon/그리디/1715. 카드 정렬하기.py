7d80e95c3f6eb594d58f78df5a2f5173222b8f1c
choihyeji <hyejichoi14@gmail.com>
2023-10-25 23:11:24 +0900

    update

---
+++ Baekjoon/그리디/1715. 카드 정렬하기.py
@@ -0,0 +1,18 @@
+'''골드 4'''
+import sys
+from queue import PriorityQueue
+n=int(sys.stdin.readline())
+que = PriorityQueue()
+for i in range(n):
+    que.put(int(sys.stdin.readline()))
+
+cnt=0
+while(1):
+    if(que.qsize()==1):
+        break
+    a=que.get()
+    b=que.get()
+    cnt+=(a+b)
+    que.put(a+b)
+
+print(cnt)
