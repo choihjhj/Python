5f140c45e100d63aaca4b155a86ab91644e01265
choihyeji <hyejichoi14@gmail.com>
2023-10-27 19:19:23 +0900

    1339 단어수학 sol, 1202보석도둑 수정

--- Baekjoon/그리디/골드/1339. 단어 수학.py
+++ Baekjoon/그리디/골드/1339. 단어 수학.py
@@ -1,5 +1,8 @@
 # 메모리: 31120 kb, 시간: 44 ms, 코드길이: 403 Bytes
-'''골드 4'''
+'''골드 4,시간제한: 2초,  N(1 ≤ N ≤ 10)
+N개의 단어가 주어졌을 때, 그 수의 합을 최대로 만드는 프로그램을 작성하시오.
+알파벳은 최대 10개이고, 수의 최대 길이는 8이다
+'''
 import sys
 input=sys.stdin.readline
 s=[input().rstrip() for _ in range(int(input()))]
@@ -13,12 +16,43 @@ for w in s:
             dict[ww]+=pow(10, cnt)
         cnt -= 1
 dict = sorted(dict.values(), reverse=True)
-
 result = 0
 num = 9
-
 for v in dict:
     result += v*num
     num -= 1
-
-print(result)           
+print(result)          
+'''
+입력예시1)
+2
+AAA
+AAA
+출력예시1)
+1998
+입력예시2)
+2
+GCF
+ACDEB
+출력예시2)
+99437
+입력예시3)
+10
+A
+B
+C
+D
+E
+F
+G
+H
+I
+J
+출력예시3)
+45
+입력예시4)
+2
+AB
+BA
+출력예시4)
+187
+''' 
