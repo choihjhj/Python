54260e9205fc8e07a3c2e409dfa2a277d1930fd8
choihyeji <hyejichoi14@gmail.com>
2023-11-01 02:25:59 +0900

    알고리즘 정리 txt 파일들을 py로 변경

--- Algorithm/브루트 포스(완전탐색).py
+++ Algorithm/브루트 포스(완전탐색).py
@@ -1,11 +1,13 @@
 # [알고리즘] 브루트 포스 알고리즘 (완전 탐색)
 
-구현방법
-1) for / while
-2) 재귀함수
+# 구현방법
+# 1) for / while
+# 2) 재귀함수
 
+'''
 프로그래머스 
 -소수 찾기 Lv2
 문제링크: https://school.programmers.co.kr/learn/courses/30/lessons/42839
 -모의고사 Lv1
 문제링크: https://school.programmers.co.kr/learn/courses/30/lessons/42840
+'''
