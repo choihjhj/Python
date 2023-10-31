51c187f5c93189c67a6ae404cf9d6cb1c48016bd
choihyeji <hyejichoi14@gmail.com>
2023-10-31 23:22:29 +0900

    1182 부분수열의합 sol

---
+++ Baekjoon/순열과조합/1182. 부분수열의 합.py
@@ -0,0 +1,15 @@
+# 메모리: 31120  kb, 시간: 344 ms, 코드길이: 274 Bytes
+'''실버2, 시간제한:2초, (1 ≤ N ≤ 20, |S| ≤ 1,000,000)
+combinations 함수를 호출해 배열에서 뽑을 수 있는 모든 조합을 구해준다.
+'''
+import sys
+from itertools import combinations
+n,s=map(int,sys.stdin.readline().split())
+arr=list(map(int,sys.stdin.readline().split()))
+cnt = 0
+for i in range(1, n+1):
+    comb = combinations(arr, i)
+    for x in comb:
+        if sum(x) == s:
+            cnt += 1
+print(cnt)
