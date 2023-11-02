a1ae104c7e853c15fcb5da9481a941b670493756
choihyeji <hyejichoi14@gmail.com>
2023-11-03 01:02:05 +0900

    BFS 미로탈출 복습

--- 이코테/DFSBFS/미로탈출.py
+++ 이코테/DFSBFS/미로탈출.py
@@ -5,11 +5,10 @@ def bfs(x,y):
     queue.append((x,y))
     while queue:
         x,y=queue.popleft()
-        for i in range(4):
-            nx=x+dx[i]
-            ny=y+dy[i]
-            if nx<0 or ny<0 or nx>=n or ny>=m:
-                continue
+        for i in range(4): #상하좌우 1 체크
+            nx=x+d[i][0]
+            ny=y+d[i][1]
+            if nx<0 or ny<0 or nx>=n or ny>=m: continue
             if graph[nx][ny]==0: continue
             if graph[nx][ny]==1: #해당노드를 처음 방문하는 경우에만 최단거리 기록
                 graph[nx][ny]=graph[x][y]+1
@@ -17,8 +16,7 @@ def bfs(x,y):
     return graph[n-1][m-1] #가장 오른쪽 아래까지의 최단거리 반환            
 n,m=map(int,sys.stdin.readline().split())
 graph=[list(map(int,sys.stdin.readline().strip())) for _ in range(n)]
-dx=[-1,1,0,0]
-dy=[0,0,-1,1]
+d=[(-1,0),(1,0),(0,-1),(0,1)] #상하좌우
 print(bfs(0,0))
 '''
 for x in graph:
